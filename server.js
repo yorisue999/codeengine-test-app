0cc217290a2df6c4af35f6fba6c02d1eeea40d02
yorisue999 <102358318+yorisue999@users.noreply.github.com>
2022-12-19 15:16:15 +0900

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 http.createServer(function (request, response) {
    
-   console.log('== codeengine-yoti-test-webapp Ver 1.0.2 ==');
+   console.log('== codeengine-yoti-test-webapp Ver 1.0.3 ==');
    console.log('@method=' + request.method);
    console.log('@path=' + request.path);
    console.log('@headers=' + request.headers);
@@ -11,9 +11,9 @@ http.createServer(function (request, response) {
    target = process.env.TARGET ? process.env.TARGET : 'World' ;
    msg = process.env.MSG ? process.env.MSG : 'Hello ' + target + '\n';
    response.writeHead(200, {'Content-Type': 'text/plain'});
-   response.end(msg + 'Ver 1.0.2');
+   response.end(msg + 'Ver 1.0.3');
 
 
 }).listen(8083);
 
-console.log('Server running at http://0.0.0.0:8083/  Ver 1.0.2');
+console.log('Server running at http://0.0.0.0:8083/  Ver 1.0.3');
